774eb0c561755b2013bf5c7dd5fc23e78813bbc2
Victor Viale <somebodywas@gmail.com>
2015-03-18 07:58:14 +0100

    fixed gitignore

--- dessinPrimitives.h
+++
@@ -1,7 +0,0 @@
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <math.h>
-
-void dessinCarre(short filled);
-void dessinRepere();
-void dessinCercle(short nbSubdiv, short filled);
--- windowManager.h
+++
@@ -1,8 +0,0 @@
-#include <SDL/SDL.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
-
-
-void reshape(int winWidth, int winHeight);
-
-void setVideoMode(int winWidth, int winHeight);
